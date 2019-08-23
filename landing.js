1122fa6af0b76898b28806c6d973c0b7e34ea4f1
walleyjs <rajiolawale2020@gmail.com>
2019-08-23 16:32:52 +0100

    finished the landing page without footer

---
+++ landing.js
@@ -0,0 +1,43 @@
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const prveBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+
+nextBtn.addEventListener("click", () => {
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    console.log(counter);
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+})
+prevBtn.addEventListener("click", () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+})
+
+carouselSlide.addEventListener("transitionend", () => {
+    if (carouselImages[counter].id === "lastclone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        console.log(counter);
+
+        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+    }
+    if (carouselImages[counter].id === "firstclone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+    }
+})
